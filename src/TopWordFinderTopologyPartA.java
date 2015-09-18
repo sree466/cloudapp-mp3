8bac553a77ebd932ac71350f606efdc9c50764a5
unknown <sreekanth_466@hotmail.com>
2015-09-18 12:22:57 -0400

    topology a use random sentense

--- src/TopWordFinderTopologyPartA.java
+++ src/TopWordFinderTopologyPartA.java
@@ -37,7 +37,7 @@ public class TopWordFinderTopologyPartA {
 
     ------------------------------------------------- */
 
-    builder.setSpout("spout", new FileReaderSpout(), 5);
+    builder.setSpout("spout", new RandomSentenceSpout(), 5);
     builder.setBolt("split", new SplitSentenceBolt(), 8).shuffleGrouping("spout");
     builder.setBolt("count", new WordCountBolt(), 12).fieldsGrouping("split", new Fields("word"));
 
