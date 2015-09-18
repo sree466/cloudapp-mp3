81d699855ef1d883ee1396d95dab210d3060a36d
unknown <sreekanth_466@hotmail.com>
2015-09-18 12:06:43 -0400

    commiting second

--- src/TopWordFinderTopologyPartD.java
+++ src/TopWordFinderTopologyPartD.java
@@ -41,16 +41,14 @@ public class TopWordFinderTopologyPartD {
 
 
     ------------------------------------------------- */
-	
-	config.put("inputFile" , args[0]);
 
-	
 	builder.setSpout("spout", new FileReaderSpout(), 1);
     builder.setBolt("split", new SplitSentenceBolt(), 5).shuffleGrouping("spout");
 	builder.setBolt("normalize", new NormalizerBolt(), 8).shuffleGrouping("split");
     builder.setBolt("count", new WordCountBolt(), 12).fieldsGrouping("normalize", new Fields("word"));
 	builder.setBolt("top-n", new TopNFinderBolt(N), 1).globalGrouping("count");
-	
+
+    config.put("inputFile" , args[0]);
 	
     config.setMaxTaskParallelism(3);
 
